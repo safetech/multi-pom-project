63e17f3b601b84ad24763061a2355de03d7c4ec6
Dodd Pfeffer <pair+dpfeffe1@uhc.com>
2015-05-19 08:50:25 -0400

    Starts California integration test through Health History

---
+++ ole-qa-ui/src/test/java/integration/phone/pages/variations/healthhistoryquestions/CAHealthHistoryQuestionsPage.java
@@ -0,0 +1,22 @@
+package integration.phone.pages.variations.healthhistoryquestions;
+
+import integration.phone.entity.Application;
+import integration.phone.pages.HealthHistoryQuestionsPage;
+import integration.phone.pages.WizardPage;
+import org.fluentlenium.core.domain.FluentWebElement;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CAHealthHistoryQuestionsPage extends WizardPage {
+
+    public void fillAndSubmit(Application app) {
+
+        clickNextAndWaitForSpinnerToFinish();
+
+    }
+
+    public void isAt() {
+        assertThat(pageTitle.getText(), equalTo("Health History Questions"));
+    }
+}
---
+++ ole-qa-ui/src/test/java/integration/phone/submission/CaliforniaIntegrationTest.java
@@ -0,0 +1,193 @@
+package integration.phone.submission;
+
+import com.github.javafaker.Faker;
+import integration.CQBaseIntegrationTest;
+import integration.phone.entity.Application;
+import integration.phone.entity.CribSheet;
+import integration.phone.entity.SubmissionResult;
+import integration.phone.pages.*;
+import integration.phone.pages.variations.healthhistoryquestions.CAHealthHistoryQuestionsPage;
+import integration.phone.pages.variations.pastandcurrentcoverage.CAEligibilityHealthQuestionsPage;
+import integration.phone.pages.variations.pastandcurrentcoverage.CAPlanApplicationQuestions;
+import integration.phone.pages.variations.pastandcurrentcoverage.PAPastAndCurrentInsuranceCoveragePage;
+import integration.phone.pages.variations.replacementnotice.RN078Page;
+import integration.phone.queries.SubmissionQuery;
+import org.fluentlenium.core.annotation.Page;
+import org.junit.Before;
+import org.junit.Test;
+import util.DateUtils;
+
+public class CaliforniaIntegrationTest extends CQBaseIntegrationTest {
+
+    @Page public CheatPage cheatPage;
+    @Page public VoiceSignatureInstructionsPage voiceSignatureInstructionsPage;
+    @Page public CustomerInformationPage customerInformationPage;
+    @Page public PlanSelectionAndStartDatePage planSelectionAndStartDatePage;
+    @Page public CAPlanApplicationQuestions planApplicationQuestionsPage;
+    @Page public CAEligibilityHealthQuestionsPage eligibilityHealthQuestionsPage;
+    @Page public CAHealthHistoryQuestionsPage healthHistoryQuestionsPage;
+    @Page public PAPastAndCurrentInsuranceCoveragePage pastAndCurrentInsuranceCoveragePage;
+    @Page public AuthorizationAndVerificationPage authorizationAndVerificationPage;
+    @Page public AgentVerificationPage agentVerificationPage;
+    @Page public RN078Page replacementNoticePage;
+    @Page public ReviewAndSubmitPage reviewAndSubmitPage;
+    @Page public ApplicationSubmissionPage applicationSubmissionPage;
+
+    public SubmissionQuery submissionQuery;
+    private Faker faker;
+    private CribSheet sheet;
+    private SubmissionResult expectedSubmissionResult;
+
+    @Before
+    public void setup() {
+        submissionQuery = new SubmissionQuery();
+        faker = new Faker();
+        sheet = new CribSheet(faker);
+        expectedSubmissionResult = new SubmissionResult();
+    }
+
+    @Test
+    public void test_california_full_underwriting_with_rn() throws Exception {
+
+        sheet.setRandomNameGenderAndMembershipNumber();
+        sheet.setRandomAddress("CA", "90210");
+        sheet.setRandomContactInfo();
+        sheet.setRandomCallCenterInfo();
+        sheet.setDateOfBirth(DateUtils.getDOBofPersonTurningAgeToday(69));
+        sheet.setMedPartBdate(DateUtils.getFirstDayOfFutureMonth(-2));
+        sheet.setDpsdToFirstDayOfFutureMonth(1);
+        sheet.setPlanCode("F01");
+
+        Application app = new Application(sheet);
+
+        app.setMedicareClaimNum(faker.bothify("??#########"));
+        app.setMPAED("01/01/2013");
+        app.setMPBED(DateUtils.convertDate(sheet.getMedPartBdate(), DateUtils.NORMALIZED_DATE_FORMAT));
+        app.setPartABActiveIndicator(YES);
+        app.setPlanCode("F");
+        app.setReqEffectiveDate(DateUtils.getFirstDayOfFutureMonth(1));
+        //Plan Eligibility
+        app.setTurned65In6GA(NO); //TODO: Replace these hard coded values with helper function that will determine answer based upon DOB
+        app.setPartBIn6GA(YES); //TODO: Replace these hard coded values with helper function that will determine answer based upon MPBED
+        app.setPlanEffIn6OfEligible(YES);  //TODO: Replace these hard coded values with helper function that will determine answer based upon DOB & MPBED
+        app.setLostCoverage(NO);
+        app.setTobaccoUse(NO);
+        app.setGI30dayBday(NO);
+        app.setGIEmployerCov(NO);
+        app.setGIMediCal(NO);
+        app.setGIMilitary(NO);
+        app.setGILocation(NO);
+        //Eligibility Questions
+        app.setESRD(NO);
+        app.setSurgeryNeeded(NO);
+        //Eligibility Questions(SPECIFIC TO CA)
+        app.setEligdialysis(NO);
+        app.setEligRecdialysis(NO);
+        app.setEligHospital(NO);
+        app.setEligSurgery(NO);
+        app.setEligOrgan(NO);
+        app.setEligSpine(NO);
+        app.setEligjoint(NO);
+        app.setEligCancer(NO);
+        app.setEligHeart(NO);
+        app.setEligVascular(NO);
+        //Health History
+        app.setEmphysema(YES);
+        app.setOthercancer(YES);
+        app.setPolycystic(YES);
+        app.setCirrhosis(YES);
+        app.setBonemarrow(YES);
+        app.setPancreatitis(YES);
+        app.setAmputation(YES);
+        app.setAlcohol(YES);
+        app.setParaplegia(YES);
+        app.setBipolar(YES);
+        app.setMacular(YES);
+        app.setAlzheimers(YES);
+        app.setHIV(YES);
+        //Past And Current Coverage
+        app.setMedicaidCovered(YES);
+        app.setMedicaidSupPremium(YES);
+        app.setMedicaidbenefit(YES);
+        app.setExistingMedicare(YES);
+        app.setOtherMedplanstart("01/01/2000");
+        app.setOtherMedplanend("01/01/2015");
+        app.setIntentReplace(YES);
+        app.setFirstTime(YES);
+        app.setDropMedSuppForThisPlan(YES);
+        app.setExistMedSupp(YES);
+        app.setReplaceExistingMedSup(YES);
+        app.setOtherInsCoverage(YES);
+        app.setOtherInsCompany("Blue Cross Blue Shield");
+        app.setOtherInsType("HMO");
+        app.setOtherInsStart("01/01/2001");
+        app.setOtherInsEnd("01/01/2014");
+        app.setOtherInsReplace(YES);
+        app.setCpaSignatureInd(YES);
+        //Agent Verification Page
+        app.setAgentOtherInsPoliciesSold("HIP");
+        app.setAgentPoliciesInForce("EP");
+        app.setAgentPoliciesSoldNotInForce("EPHIP");
+        app.setAgentSignatureInd(YES);
+        //Replacement Notice Page
+        app.setReplacementReason("OtherReason");
+        app.setRNOther("Cheaper");
+        app.setAgentPrintedNameAdd("ProducerName");
+        app.setAgentAddress("ProducerAdd");
+        app.setApplicantPrintedNameAdd("AppName");
+        app.setApplicantAddress("AppAdd");
+
+
+        expectedSubmissionResult.setAdjudicationStatus("P");
+        expectedSubmissionResult.setStatus("C");
+        expectedSubmissionResult.setWorkQueue("UNDERWRITING");
+        expectedSubmissionResult.setWorkQueueReason("REVIEW FOR POSSIBLE ESRD");
+
+        logger.info(gson.toJson(app));
+
+        goTo(cheatPage);
+        cheatPage.isAt();
+        cheatPage.fillAndSubmit(sheet);
+
+        voiceSignatureInstructionsPage.isAt();
+        voiceSignatureInstructionsPage.fillAndSubmit(app);
+
+        customerInformationPage.isAt();
+        customerInformationPage.fillAndSubmit(app);
+
+        planSelectionAndStartDatePage.isAt();
+        planSelectionAndStartDatePage.fillAndSubmit(app);
+
+        planApplicationQuestionsPage.isAt();
+        planApplicationQuestionsPage.fillAndSubmit(app);
+
+        eligibilityHealthQuestionsPage.isAt();
+        eligibilityHealthQuestionsPage.fillAndSubmit(app);
+
+        healthHistoryQuestionsPage.isAt();
+        healthHistoryQuestionsPage.fillAndSubmit(app);
+
+        pastAndCurrentInsuranceCoveragePage.isAt();
+        pastAndCurrentInsuranceCoveragePage.fillAndSubmit(app);
+
+        authorizationAndVerificationPage.isAt();
+        authorizationAndVerificationPage.fillAndSubmit(app);
+
+        agentVerificationPage.isAt();
+        agentVerificationPage.fillAndSubmit(app);
+
+        replacementNoticePage.isAt();
+        replacementNoticePage.fillAndSubmit(app);
+
+        reviewAndSubmitPage.isAt();
+        reviewAndSubmitPage.fillAndSubmit(app);
+
+        applicationSubmissionPage.isAt();
+        applicationSubmissionPage.isPending();
+
+        submissionQuery.verifySubmissionData(app, expectedSubmissionResult);
+        submissionQuery.verifyAdjudicationData(app, expectedSubmissionResult);
+
+    }
+
+}
