11dc5ffdf180ad76bb896f68694f8eac5b833a7d
sislam13 <s_islam@uhc.com>
2015-06-09 20:15:22 -0400

    Massachusetts in progress

---
+++ ole-qa-ui/src/test/java/integration/phone/submission/MassachusettsIntegrationTest.java
@@ -0,0 +1,203 @@
+package integration.phone.submission;
+
+import com.github.javafaker.Faker;
+import integration.CQBaseIntegrationTest;
+import integration.phone.entity.Application;
+import integration.phone.entity.CribSheet;
+import integration.phone.entity.SubmissionResult;
+import integration.phone.pages.*;
+import integration.phone.pages.variations.pastandcurrentcoverage.CTPastAndCurrentInsuranceCoveragePage;
+import integration.phone.pages.variations.replacementnotice.RN034andRE073Page;
+import integration.phone.queries.SubmissionQuery;
+import org.fluentlenium.core.annotation.Page;
+import org.junit.Before;
+import org.junit.Test;
+import util.DateUtils;
+
+public class MassachusettsIntegrationTest extends CQBaseIntegrationTest {
+
+    @Page public CheatPage cheatPage;
+    @Page public VoiceSignatureInstructionsPage voiceSignatureInstructionsPage;
+    @Page public CustomerInformationPage customerInformationPage;
+    @Page public PlanSelectionAndStartDatePage planSelectionAndStartDatePage;
+    @Page public CTPastAndCurrentInsuranceCoveragePage pastAndCurrentInsuranceCoveragePage;
+    @Page public AuthorizationAndVerificationPage authorizationAndVerificationPage;
+    @Page public AgentVerificationPage agentVerificationPage;
+    @Page public RN034andRE073Page ReplacementNotice034Page;
+    @Page public ReviewAndSubmitPage reviewAndSubmitPage;
+    @Page public ApplicationSubmissionPage applicationSubmissionPage;
+
+    public SubmissionQuery submissionQuery;
+    private Faker faker;
+    private CribSheet sheet;
+    private SubmissionResult expectedSubmissionResult;
+
+    @Before
+    public void setup() {
+        submissionQuery = new SubmissionQuery();
+        faker = new Faker();
+        sheet = new CribSheet(faker);
+        expectedSubmissionResult = new SubmissionResult();
+    }
+
+    @Test
+    public void test_db() throws Exception {
+
+        Application app = new Application();
+        app.setHCSGApplicationId("3-BXRJHQO");
+        SubmissionResult expectedSubmissionResult = new SubmissionResult();
+        submissionQuery.verifySubmissionData(app, expectedSubmissionResult);
+    }
+
+    @Test
+    public void test_connecticut_guaranteed_issue_without_rn() throws Exception {
+
+        sheet.setRandomNameGenderAndMembershipNumber();
+        sheet.setRandomAddress("CT", "06230");
+        sheet.setRandomContactInfo();
+        sheet.setRandomCallCenterInfo();
+        sheet.setDateOfBirth(DateUtils.getDOBofPersonTurningAgeToday(65));
+        sheet.setMedPartBdate("1999-01-01");
+        sheet.setDpsdToFirstDayOfFutureMonth(1);
+        sheet.setPlanCode("A01");
+
+        Application app = new Application(sheet);
+
+        app.setMedicareClaimNum(faker.bothify("#########A"));
+        app.setMPAED("01/01/1999");
+        app.setMPBED("01/01/1999");
+        app.setPlanCode("A");
+        app.setReqEffectiveDate(DateUtils.getFirstDayOfFutureMonth(1));
+        app.setCpaSignatureInd(YES);
+        app.setPartABActiveIndicator(YES);
+        app.setCPATurned65(YES);
+        app.setMedicaidSupPremium(YES);
+        app.setCPAPartBIn6(NO);
+        app.setMedicaidCovered(NO);
+        app.setExistingMedicare(NO);
+        app.setExistMedSupp(NO);
+        app.setOtherInsCoverage(NO);
+        app.setTobaccoUse(NO);
+        app.setOtherMedplanstart("");
+        app.setOtherMedplanend("");
+        app.setAgentOtherInsPoliciesSold("HIP");
+        app.setAgentPoliciesInForce("EP");
+        app.setAgentPoliciesSoldNotInForce("EPHIP");
+        app.setAgentSignatureInd(YES);
+
+        expectedSubmissionResult.setStatus("C");
+        expectedSubmissionResult.setAdjudicationStatus("A");
+
+        goTo(cheatPage);
+        cheatPage.isAt();
+        cheatPage.fillAndSubmit(sheet);
+
+        voiceSignatureInstructionsPage.isAt();
+        voiceSignatureInstructionsPage.fillAndSubmit(app);
+
+        customerInformationPage.isAt();
+        customerInformationPage.fillAndSubmit(app);
+
+        planSelectionAndStartDatePage.isAt();
+        planSelectionAndStartDatePage.fillAndSubmit(app);
+
+        pastAndCurrentInsuranceCoveragePage.isAt();
+        pastAndCurrentInsuranceCoveragePage.fillAndSubmit(app);
+
+        authorizationAndVerificationPage.isAt();
+        authorizationAndVerificationPage.fillAndSubmit(app);
+
+        agentVerificationPage.isAt();
+        agentVerificationPage.fillAndSubmit(app);
+
+        reviewAndSubmitPage.isAt();
+        reviewAndSubmitPage.fillAndSubmit(app);
+
+        applicationSubmissionPage.isAt();
+        applicationSubmissionPage.isApproved();
+
+        submissionQuery.verifySubmissionData(app, expectedSubmissionResult);
+        submissionQuery.verifyAdjudicationData(app, expectedSubmissionResult);
+
+    }
+
+    @Test
+    public void test_connecticut_guaranteed_issue_with_rn() throws Exception {
+
+        sheet.setRandomNameGenderAndMembershipNumber();
+        sheet.setRandomAddress("CT", "06230");
+        sheet.setRandomContactInfo();
+        sheet.setRandomCallCenterInfo();
+        sheet.setDateOfBirth(DateUtils.getDOBofPersonTurningAgeToday(65));
+        sheet.setMedPartBdate("1999-01-01");
+        sheet.setDpsdToFirstDayOfFutureMonth(1);
+        sheet.setPlanCode("A01");
+
+        Application app = new Application(sheet);
+
+        app.setMedicareClaimNum(faker.bothify("??#########"));
+        app.setMPAED("01/01/1999");
+        app.setMPBED("01/01/1999");
+        app.setPartABActiveIndicator(YES);
+        app.setPlanCode("A");
+        app.setReqEffectiveDate(DateUtils.getFirstDayOfFutureMonth(1));
+        app.setTobaccoUse(NO);
+        app.setCPATurned65(YES);
+        app.setExistingMedicare(NO);
+        app.setCPAPartBIn6(NO);
+        app.setMedicaidCovered(NO);
+        app.setOtherMedplanstart("");
+        app.setOtherMedplanend("");
+        app.setExistMedSupp(YES);
+        app.setReplaceExistingMedSup(YES);
+        app.setOtherInsCoverage(NO);
+        app.setAgentSignatureInd(YES);
+        app.setCpaSignatureInd(YES);
+        app.setAgentOtherInsPoliciesSold("HIP");
+        app.setAgentPoliciesInForce("EP");
+        app.setAgentPoliciesSoldNotInForce("EPHIP");
+        app.setReplacementReason("OtherReason");
+        app.setApplicantPrintedNameAdd("AppName");
+        app.setApplicantAddress("AppAdd");
+        app.setRNOther("Cheaper");
+
+        expectedSubmissionResult.setStatus("C");
+        expectedSubmissionResult.setAdjudicationStatus("A");
+
+        goTo(cheatPage);
+        cheatPage.isAt();
+        cheatPage.fillAndSubmit(sheet);
+
+        voiceSignatureInstructionsPage.isAt();
+        voiceSignatureInstructionsPage.fillAndSubmit(app);
+
+        customerInformationPage.isAt();
+        customerInformationPage.fillAndSubmit(app);
+
+        planSelectionAndStartDatePage.isAt();
+        planSelectionAndStartDatePage.fillAndSubmit(app);
+
+        pastAndCurrentInsuranceCoveragePage.isAt();
+        pastAndCurrentInsuranceCoveragePage.fillAndSubmit(app);
+
+        authorizationAndVerificationPage.isAt();
+        authorizationAndVerificationPage.fillAndSubmit(app);
+
+        agentVerificationPage.isAt();
+        agentVerificationPage.fillAndSubmit(app);
+
+        ReplacementNotice034Page.isAt();
+        ReplacementNotice034Page.fillAndSubmit(app);
+
+        reviewAndSubmitPage.isAt();
+        reviewAndSubmitPage.fillAndSubmit(app);
+
+        applicationSubmissionPage.isAt();
+        applicationSubmissionPage.isApproved();
+
+        submissionQuery.verifySubmissionData(app, expectedSubmissionResult);
+        submissionQuery.verifyAdjudicationData(app, expectedSubmissionResult);
+
+    }
+
+}
